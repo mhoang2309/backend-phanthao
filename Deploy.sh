db5ebf38612eecc358787253c1953e741e01e43a
mhoang2309 <mhoang2309@gmail.com>
2024-03-22 14:05:18 +0700

    fix: deploy.sh

--- Deploy.sh
+++ Deploy.sh
@@ -1,8 +1,4 @@
 
 source config/config.cfg
 
-<<<<<<< HEAD
 uvicorn app.main:app --host $host --port $port --workers $workers
-=======
-uvicorn app.main:app --host $host --port $port --workers $workers
->>>>>>> b445676237800b9fcee4fb02e59bd54a2e21b037
