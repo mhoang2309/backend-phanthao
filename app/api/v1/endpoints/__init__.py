bec67edd208378638dbe9767aaaa7629495f6a92
Nguyen Manh Hoang <56344105+mhoang2309@users.noreply.github.com>
2024-03-22 03:27:11 +0000

    Revert "new:Add new base structure"
    This reverts commit b445676237800b9fcee4fb02e59bd54a2e21b037.

--- app/api/v1/endpoints/__init__.py
+++
@@ -1,1 +0,0 @@
-from app.api.v1.endpoints.web_admin.views import *
--- app/api/v1/routers.py
+++
@@ -1,7 +0,0 @@
-from fastapi import APIRouter
-
-from app.api.v1.endpoints import api_user
-
-api = APIRouter()
-
-api.include_router(api_user, tags=["API Manage"])
