09e19b7185a45eb3bf7fa496eae5971216e97e60
mhoang2309 <mhoang2309@gmail.com>
2024-03-22 12:27:07 +0700

    new: Add http

--- app/api/base/errors/http_error.py
+++ app/api/base/errors/http_error.py
@@ -5,27 +5,8 @@ from starlette.responses import JSONResponse
 
 templates = Jinja2Templates(directory="./app/static/templates")
 
-<<<<<<< HEAD
-async def http_error_handler(_: Request, exc: HTTPException) -> JSONResponse:
-    if isinstance(exc.detail, dict):
-        content = {
-            'error_code': exc.detail['error_code'],
-            'description': exc.detail['description']
-        }
-    else:
-        content = {
-            'error_code': None,
-            'description': exc.detail
-        }
-        
-    return JSONResponse(
-        content=content,
-        status_code=exc.status_code
-    )
-    
-=======
 
-async def http_error_handler(request: Request, exc: HTTPException) -> JSONResponse:
+async def http_error_handler(_: Request, exc: HTTPException) -> JSONResponse:
     if isinstance(exc.detail, dict):
         content = {
             "error_code": exc.detail["error_code"],
@@ -37,7 +18,6 @@ async def http_error_handler(request: Request, exc: HTTPException) -> JSONRespon
     return JSONResponse(content=content, status_code=exc.status_code)
 
 
->>>>>>> b445676237800b9fcee4fb02e59bd54a2e21b037
 def http_error_handler_templates(request: Request, exc: HTTPException):
     return templates.TemplateResponse(
         "error_page.html",
@@ -48,8 +28,4 @@ def http_error_handler_templates(request: Request, exc: HTTPException):
             "error_message": "Look like you're lost the page you are looking for not avaible!",
         },
         status_code=exc.status_code,
-<<<<<<< HEAD
     )
-=======
-    )
->>>>>>> b445676237800b9fcee4fb02e59bd54a2e21b037
--- app/settings/events.py
+++ app/settings/events.py
@@ -1,14 +1,3 @@
-<<<<<<< HEAD
-import redis
-import pymongo
-from typing import Callable
-
-from loguru import logger
-from fastapi import FastAPI
-
-from app.settings.config import redis_config 
-from app.settings.config import mongodb_config
-=======
 from typing import Callable
 
 import pymongo
@@ -19,20 +8,12 @@ from loguru import logger
 from app.settings.config import mongodb_config
 from app.settings.config import redis_config
 
->>>>>>> b445676237800b9fcee4fb02e59bd54a2e21b037
 
 def conect_mongodb(app: FastAPI):
     logger.info("Connecting to MongoDB")
     app.state.mongodb = pymongo.MongoClient(mongodb_config.MONGODB_URL)
     app.state.mongodb.server_info()
 
-<<<<<<< HEAD
-def conect_redis(app: FastAPI):
-    logger.info("Connecting to Redis")
-    app.state.redis = redis.Redis(host=redis_config.HOST_REDIS, port=redis_config.PORT_REDIS, password=redis_config.PASSWORD_REDIS)
-    app.state.redis.ping()
-
-=======
 
 def conect_redis(app: FastAPI):
     logger.info("Connecting to Redis")
@@ -44,39 +25,26 @@ def conect_redis(app: FastAPI):
     app.state.redis.ping()
 
 
->>>>>>> b445676237800b9fcee4fb02e59bd54a2e21b037
 def close_mongodb(app: FastAPI):
     logger.info("Closing connection to redis")
     app.state.redis.close()
     logger.info("Connection closed")
-<<<<<<< HEAD
-    
-=======
 
 
->>>>>>> b445676237800b9fcee4fb02e59bd54a2e21b037
 def close_redis(app: FastAPI):
     logger.info("Closing connection to mongodb")
     app.state.mongodb.close()
     logger.info("Connection closed")
 
-<<<<<<< HEAD
-=======
 
->>>>>>> b445676237800b9fcee4fb02e59bd54a2e21b037
 def create_start_app_handler(app: FastAPI) -> Callable:  # type: ignore
     async def start_app():
         conect_redis(app)
         conect_mongodb(app)
-<<<<<<< HEAD
-    return start_app
-
-=======
 
     return start_app
 
 
->>>>>>> b445676237800b9fcee4fb02e59bd54a2e21b037
 def create_stop_app_handler(app: FastAPI) -> Callable:  # type: ignore
     @logger.catch
     async def stop_app() -> None:
@@ -86,9 +54,5 @@ def create_stop_app_handler(app: FastAPI) -> Callable:  # type: ignore
         """
         close_redis(app)
         close_mongodb(app)
-<<<<<<< HEAD
-    return stop_app
-=======
 
     return stop_app
->>>>>>> b445676237800b9fcee4fb02e59bd54a2e21b037
